a40046d6647b5a2369a81d11bb2ff57c81c8bcdc
Đạt Nguyễn <ntdat@Dats-MacBook-Pro.local>
2021-05-10 10:01:01 +0700

    first commit polynomial

--- TestH264Video.py
+++ TestH264Video.py
@@ -4,31 +4,44 @@ import matplotlib.pyplot as plt
 from time import sleep
 import os
 
+PATH_TO_DIR = "/Users/ntdat/Tài liệu/Nghiên cứu nhận dạng khuôn mặt/Data/"
 SIZE_W = 500
-count = 0
 
-test_img = cv2.imread("../input/cityscapes-image-pairs/cityscapes_data/train/1.jpg")
-plt.imshow(test_img)
-plt.show()
+list_dir = os.listdir(PATH_TO_DIR)
 
-cap = cv2.VideoCapture(
-    "../input/data-nckh-facerecognition/102180162.h264")
+for video in list_dir:
+    if "Test" in video:
+        continue
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    height, width, channel = frame.shape
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = cv2.resize(frame, (SIZE_W, int(height/width*SIZE_W)))
-    print((int(width/height*SIZE_W), SIZE_W))
-    cv2.imwrite("./image_" + str(count) + ".jpg", frame)
-    count+=1
+    video_name = video.split(".")[0]
 
-    # gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if os.path.exists(PATH_TO_DIR + video_name) == False:
+        os.mkdir(PATH_TO_DIR + video_name)
 
-    plt.imshow(frame)
-    plt.show()
-    sleep(0.1)
-    if 0xFF == ord('q'):
-        break
+    count = 0
+    count_frame = 0
 
-cap.release()
+    cap = cv2.VideoCapture(PATH_TO_DIR + video)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        height, width, channel = frame.shape
+
+        if count_frame % 20 == 0:
+            print("./" + video_name + "/image_" +
+                  str(count) + ".jpg")
+            cv2.imwrite(PATH_TO_DIR + video_name + "/image_" +
+                        str(count) + ".jpg", frame)
+            count += 1
+        count_frame += 1
+
+        # gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # plt.imshow(frame)
+        # plt.show()
+        # sleep(0.1)
+        if 0xFF == ord('q'):
+            break
+
+    cap.release()
